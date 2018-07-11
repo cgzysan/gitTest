dbf8dc43906e889485d2ce505fb90522d79d0593
cgzysan <cgzysan@icloud.com>
2018-07-11 15:53:41 +0800

    aa 再一次修改在 16点

--- aa.java
+++ aa.java
@@ -1,3 +1,4 @@
-private static final int run_i = 1;
-private int age = 21;
+private static final int find_i = 1;
+private static int my_age = 21;
+public static final String name = "zhaowei";
 private static final String DD = "SDF_MODE";
